ba5c8cac527208fdd0d84d87f12d33885102fbf8
Wojicech Slusarczyk <wojciech.oro.slusarczyk@gmail.com>
2025-04-12 01:23:51 +0200

    feat: add initial Spring Boot server setup with Maven wrapper and configuration files

---
+++ be/dailogi-server/src/test/java/com/github/vvojtas/dailogi_server/DailogiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vvojtas.dailogi_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailogiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
